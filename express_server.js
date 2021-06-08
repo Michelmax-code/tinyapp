65044f45e0d2612c6b5a0233e5bf274decc70efe
Miguel Jimenez <migang9@gmail.com>
2021-06-07 21:24:04 -0400

    short and long templates

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -24,3 +26,15 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let temp = req.params.shortURL; // temp will have the value of shortURL, which is what we type in browser after /urls/:
+  const templateVars = { shortURL: temp, longURL: urlDatabase[temp] };
+  res.render("urls_show", templateVars);
+});
+
