4d5359634ea84ff5f82be4416912d7403ed2d1a3
Miguel Jimenez <migang9@gmail.com>
2021-06-12 12:20:03 -0400

    encrypting cookies

--- express_server.js
+++ express_server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
 app.set("view engine", "ejs");
 
 const generateRandomString = function() {
@@ -20,9 +23,9 @@ const urlDatabase = {
 };
 
 const users = {
-  "maj123": {id: "maj123", email: "migang9@gmail.com", password: "$2b$10$rFM9FmoBT6m5OrxsgGr.ouWRnH9Ufgc6hDEKn7pgnwHhlBTGHWsgC"},
-  "maj456": {id: "maj456", email: "majs8323@gmail.com", password: "$2b$10$1xHPOK3EVC76gSQRbffeV.8AkDb79Lf67bPbT9y1c0oWjmJCXy.kS"}
-
+  "maj123": {id: "maj123", email: "migang9@gmail.com", password: bcrypt.hashSync('123', saltRounds)},
+  "maj456": {id: "maj456", email: "majs8323@gmail.com", password: bcrypt.hashSync('12', saltRounds)}
+//"$2b$10$rFM9FmoBT6m5OrxsgGr.ouWRnH9Ufgc6hDEKn7pgnwHhlBTGHWsgC"//"$2b$10$1xHPOK3EVC76gSQRbffeV.8AkDb79Lf67bPbT9y1c0oWjmJCXy.kS"
 };
 
 // create new user
@@ -62,7 +65,7 @@ const urlsForUser = (id, urlDatabase) => {
 
 //For new registration - page to register
 app.get('/register', (req, res) => {
-  const templateVars = { username: users[req.cookies["user_id"]]};
+  const templateVars = { username: users[req.session["user_id"]]};
   res.render('urls_register', templateVars);
 });
 // post the register
@@ -76,7 +79,7 @@ app.post("/register", (req, res) => {
   }
   if (!user) {
     const userId = addNewUser(email, password);
-    res.cookie('user_id', userId);
+    req.session['user_id'] = userId;
     res.redirect("/urls");
   } else {
     res.status(403).send('You have to use another combination!');
@@ -91,7 +94,7 @@ app.post("/register", (req, res) => {
 });
 // login section
 app.get('/login', (req, res) => {
-  let templateVars = {username: users[req.cookies['user_id']], users};
+  let templateVars = {username: users[req.session['user_id']], users};
   res.render('urls_login', templateVars);
 });
 
@@ -99,7 +102,7 @@ app.post('/login', (req, res) => {
   let user = findUserByEmail(req.body.email, users);
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
   //if (user && user.password === req.body.password) {
-    res.cookie('user_id', user.id);
+    req.session['user_id'] = user.id;
     res.redirect('/urls');
   } else {
     res.send('403: Forbidden Error', 403);
@@ -108,14 +111,14 @@ app.post('/login', (req, res) => {
 
 //Show urls page with the urls
 app.get('/urls', (req, res) => {
-  const userLogged = users[req.cookies["user_id"]];
+  const userLogged = users[req.session["user_id"]];
   console.log(userLogged);
   if (!userLogged) {
     res.send('First, Login or Register, thanks!!');
     return;
   }
-  const urls = urlsForUser(req.cookies["user_id"], urlDatabase);
-  const templateVars = { username: users[req.cookies["user_id"]], urls};
+  const urls = urlsForUser(req.session["user_id"], urlDatabase);
+  const templateVars = { username: users[req.session["user_id"]], urls};
   res.render('urls_index', templateVars);
 });
 
@@ -133,25 +136,25 @@ app.get("/hello", (req, res) => {
 //
 app.get("/urls/new", (req, res) => {
 //const userLogged = {username: users[req.cookies["user_id"]]}; wrong!!
-  const userLogged = users[req.cookies["user_id"]];
+  const userLogged = users[req.session["user_id"]];
   console.log(userLogged);
   if (!userLogged) {
     res.redirect('/login');
     return;
   }
-  const templateVars = {username: users[req.cookies["user_id"]]};
+  const templateVars = {username: users[req.session["user_id"]]};
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const userLogged = users[req.cookies["user_id"]];
+  const userLogged = users[req.session["user_id"]];
   console.log(userLogged);
   if (!userLogged) {
     res.redirect('/login');
     return;
   }
   let temp = req.params.shortURL; // temp will have the value of shortURL, which is what we type in browser after /urls/:
-  const templateVars = { shortURL: temp, longURL: urlDatabase[temp]["longURL"], username: users[req.cookies["user_id"]]};
+  const templateVars = { shortURL: temp, longURL: urlDatabase[temp]["longURL"], username: users[req.session["user_id"]]};
   res.render("urls_show", templateVars);
 });
 
@@ -160,7 +163,7 @@ app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = {
     longURL: longURL,
-    userId: users[req.cookies["user_id"]]
+    userId: users[req.session["user_id"]]
   };
   console.log(urlDatabase);  // Log the POST request body to the console
   res.redirect("/urls"); // Respond with 'Ok' (we will replace this)
@@ -173,7 +176,7 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  const userLogged = [req.cookies["user_id"]];
+  const userLogged = [req.session["user_id"]];
   let urlDel = req.params.shortURL;
   if (!userLogged) {
     return res.redirect('/login');
@@ -188,12 +191,12 @@ app.post("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
   urlDatabase[shortURL] = {
     longURL: req.body.longURL,
-    userId: users[req.cookies["user_id"]]};
+    userId: users[req.session["user_id"]]};
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie("user_id");
+  req.session["user_id"] = null;
   res.redirect("/login");
 });
 
