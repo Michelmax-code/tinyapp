ac0153596ecd8df6ebcd8d5148b497641d1bef80
Miguel Jimenez <migang9@gmail.com>
2021-06-12 13:26:36 -0400

    created UserByEmail function

---
+++ helpers.js
@@ -0,0 +1,14 @@
+
+
+//function to search the email in users object
+const findUserByEmail = (email, users) => {
+  for (let user of Object.keys(users)) {
+    if (users[user].email === email) {
+      return users[user];
+    }
+  }
+  return false;
+};
+
+
+module.exports = { findUserByEmail };
